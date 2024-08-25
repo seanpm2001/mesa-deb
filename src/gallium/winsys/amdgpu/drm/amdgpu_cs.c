b02c1a082254c1d3b0eed933f14c90824301c75e
Leonardo Held <leonardo.held@toradex.com>
2024-08-25 15:40:04 -0300

    Revert "winsys/amdgpu: remove amdgpu_bo_real::gpu_address, use amdgpu_va_get_start_addr"
    This reverts commit d05e029ff0c07fff3f02f9f6553857131191fb51.

--- src/gallium/winsys/amdgpu/drm/amdgpu_cs.c
+++ src/gallium/winsys/amdgpu/drm/amdgpu_cs.c
@@ -1146,8 +1146,7 @@ static unsigned amdgpu_cs_get_buffer_list(struct radeon_cmdbuf *rcs,
     if (list) {
         for (unsigned i = 0; i < num_real_buffers; i++) {
             list[i].bo_size = real_buffers->buffers[i].bo->base.size;
-            list[i].vm_address =
-               amdgpu_va_get_start_addr(get_real_bo(real_buffers->buffers[i].bo)->va_handle);
+            list[i].vm_address = get_real_bo(real_buffers->buffers[i].bo)->gpu_address;
             list[i].priority_usage = real_buffers->buffers[i].usage;
         }
     }
